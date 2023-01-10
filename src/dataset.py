eca1b889218ea6f6e27e23261a015427df92f5fa
aneesh-aparajit <aneeshaparajit.g2002@gmail.com>
2023-01-10 08:31:32 +0530

    resolved bugs in dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -8,83 +8,61 @@ import config
 from typing import Dict, List
 from pprint import pprint
 
+
 class TweetDataset(Dataset):
-    def __init__(self, text: List[str], selected_text: List[str], sentiment: List[str]) -> None:
+    def __init__(self, texts: List[str], contexts: List[str], sentiments: List[str]) -> None:
         super(TweetDataset, self).__init__()
-        self.text = text
-        self.selected_text = selected_text
-        self.sentiment = sentiment
+        self.texts = texts
+        self.contexts = contexts
+        self.sentiments = sentiments
         self.max_len = config.max_len
-    
+
     def __len__(self) -> int:
-        return len(self.text)
-    
-    def __getitem__(self, ix) -> Dict[str, T.Tensor]:
-        text = self.text[ix].split()
-        context = self.selected_text[ix].split()
-        sentiment = self.sentiment[ix]
+        return len(self.texts)
 
-        '''
-        Now we need to split the string into words and keep track of the words which belong to the selected text. 
-            - One thing to notice here is that, here we include even the part is not in the continuous subarray, 
-                - we go by the assumption that both may have the similar intent in the sentiment.
-        - Firstly, start out by finding the start and end index of the selected text in the actual text.
-            - it's better if we use the word based.
-        '''
-        start_ix, end_ix = 0, len(text) - 1
-        len_context = len(context)
-        for ix in range(len(text)):
-            if text[ix] == context[0]:
-                if text[ix:ix+len_context] == context:
-                    start_ix = ix
-                    end_ix = ix + len_context - 1
+    def __getitem__(self, ix: int) -> Dict[str, T.Tensor]:
+        text = self.texts[ix]
+        context = self.contexts[ix]
+        sentiment = self.sentiments[ix]
 
-        print(f'start: {start_ix}')
-        print(f'end: {end_ix}')
-        
-        input_ids = []
-        attention_mask = []
-        token_type_ids = []
-        
-        for ix,  word in enumerate(text):
-            word_tok = config.tokenizer(word, add_special_tokens=False)
-            len_tok = len(word_tok['input_ids'])
-            input_ids.extend(word_tok['input_ids'])
-            attention_mask.extend(word_tok['attention_mask'])
-            token_type_ids.extend(word_tok['token_type_ids'])
-            if ix < start_ix:
-                start_ix += (len_tok - 1)
-                end_ix += (len_tok - 1)
-            elif ix < end_ix:
-                end_ix += (len_tok - 1)
-            else:
-                continue
-            print(f'start: {start_ix} end_ix: {end_ix}')
-        
-        enc_sentiment = [1, 0, 0] if sentiment == 'negative' else [0, 1, 0] if sentiment == 'neutral' else [0, 0, 1]
+        sentiment_tensor = [0, 1, 0]
+        if sentiment == 'negative':
+            sentiment_tensor = [1, 0, 0]
+        elif sentiment == 'positive':
+            sentiment_tensor = [0, 0, 1]
 
-        input_ids = [101] + input_ids + [102]
-        token_type_ids = [0] + token_type_ids + [0]
-        attention_mask = [1] + attention_mask + [1]
-        
-        padding_len = config.max_len - len(input_ids)
-        token_type_ids.extend([0] * padding_len)
-        attention_mask.extend([0] * padding_len)
+        text_tok = config.tokenizer.encode(text, add_special_tokens=False)
+        context_tok = config.tokenizer.encode(context, add_special_tokens=False)
+
+        start_ix, end_ix = -1, -1
+
+        for i in range(len(text_tok)):
+            if text_tok[i] == context_tok[i]:
+                if text_tok[i:i+len(context_tok)] == context_tok:
+                    start_ix = i
+                    end_ix = i + len(context_tok) - 1
+                    break
+
+        input_ids = [101] + text_tok + [102]
+        attention_mask = [1] * len(input_ids)
+        token_type_ids = [0] * len(input_ids)
+
+        # padding
+        padding_len = self.max_len - len(input_ids)
         input_ids.extend([0] * padding_len)
+        attention_mask.extend([0] * padding_len)
+        token_type_ids.extend([0] * padding_len)
 
         return {
+            'text_tok': T.tensor(text_tok, dtype=T.long),
+            'context_tok': T.tensor(context_tok, dtype=T.long),
+            'sentiment': T.tensor(sentiment_tensor, dtype=T.long),
+            'start': start_ix,
+            'end': end_ix,
             'input_ids': T.tensor(input_ids, dtype=T.long),
-            'token_type_ids': T.tensor(token_type_ids, dtype=T.long),
-            'attention_mask': T.tensor(attention_mask, dtype=T.long), 
-            'start': T.tensor(start_ix, dtype=T.long),
-            'end': T.tensor(end_ix, dtype=T.long), 
-            'sentiment': T.tensor(enc_sentiment, dtype=T.long), 
-            'orig_sentiment': sentiment, 
-            'text':  ' '.join(text),
-            'context': ' '.join(context),
-            'padding_len': padding_len
+            'attention_mask': T.tensor(attention_mask, dtype=T.long),
+            'token_type_ids': T.tensor(token_type_ids, dtype=T.long)
         }
-    
 
 
 if __name__ == '__main__':
---
+++ src/model.py
@@ -0,0 +1,11 @@
+import torch as T
+from torch import nn as nn, optim as optim
+import torch.nn.functional as F
+
+import config
+
+
+class TweetModel(nn.Module):
+    def __init__(self, max_pos: int) -> None:
+        pass
+
